44c679003a21fdcb6b30d586ebddde4c8071a4a7
LoadWithWater <rlarhkdwo98@naver.com>
2026-05-06 00:03:11 +0900

    Create: MainMenu

--- NewScript.cs
+++ NewScript.cs
@@ -1,15 +1,23 @@
 using Godot;
 using System;
 
-public partial class NewScript : Node
+public partial class MainMenu : Control
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// 버튼을 찾아서 클릭 이벤트를 연결합니다.
+		GetNode<Button>("StartButton").Pressed += OnStartButtonPressed;
+		GetNode<Button>("QuitButton").Pressed += OnQuitButtonPressed;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void OnStartButtonPressed()
 	{
+		GD.Print("게임 시작!");
+		// 여기에 다음 화면으로 넘어가는 코드를 나중에 추가합니다.
+	}
+
+	private void OnQuitButtonPressed()
+	{
+		GetTree().Quit(); // 프로그램 종료
 	}
 }
